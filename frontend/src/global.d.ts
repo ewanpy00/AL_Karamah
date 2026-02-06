689ac03c5eddec5970fff039bf95dce55cbd7bf3
Martin Mustonen <mmartinki@gmail.com>
2026-02-07 01:36:39 +0400

    del big files

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,20 @@
+declare module '*.css'
+declare module '*.scss'
+declare module '*.sass'
+declare module '*.less'
+declare module '*.styl'
+
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.svg'
+declare module '*.webp'
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
